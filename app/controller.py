5a72326471d8f1b326e285964e0530bb3e2b6ca2
akshatcx <akshat.c2k@gmail.com>
2019-04-11 19:39:48 +0530

    [Addition] - Added API to the controller

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from .api import helper
 #from .models import db
 
 import json
@@ -41,6 +42,54 @@ def feedback():
     return render_template("Feedback.html")
     pass
 
-@main.route('/api')
-def api():
-    pass
+@main.route('/api/p')
+def p():
+    l=0
+    l=int(str(request.args.get('bits')))
+    if not l:
+        l=256
+    p=helper.genp(l)
+    return json.dumps({'prime': p })
+
+@main.route('/api/g')
+def g():
+    l=0
+    l=int(str(request.args.get('bits')))
+    if not l:
+        l=256
+    g=helper.geng(l)
+    return json.dumps({'generator': g })
+
+@main.route('/api/private_key')
+def private_key():
+    p=int(str(request.args.get('prime')))
+    if not p:
+        return "Invalid"
+    pk=helper.genpk(p)
+    return json.dumps({'private_key' : pk })
+
+@main.route('/api/calg')
+def calg():
+    
+    p=int(str(request.args.get('p')))
+    a=int(str(request.args.get('a')))
+    b=int(str(request.args.get('b')))
+    g=int(str(request.args.get('g')))
+
+    Ga = helper.calg(g,a,p)
+    Gb = helper.calg(g,b,p)
+    Gab = helper.calg(Gb,a,p)
+    Gba = helper.calg(Ga,b,p)
+
+    ret = {
+        'Ga' : Ga,
+        'Gb' : Gb,
+        'Gab' : Gab,
+        'Gba' : Gba,
+    }
+
+    return json.dumps(ret)
+
+
+
+
